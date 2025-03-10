ee6e02df74600297e4b9bc14fdf4fd03fb90a339
newlandslucas <lucasnewlands.m@gmail.com>
2025-03-10 17:44:24 -0300

    Initial Commit

---
+++ BudgetPlannet/BudgetPlannetApp.swift
@@ -0,0 +1,17 @@
+//
+//  BudgetPlannetApp.swift
+//  BudgetPlannet
+//
+//  Created by Lucas Newlands on 10/03/25.
+//
+
+import SwiftUI
+
+@main
+struct BudgetPlannetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
